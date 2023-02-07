7313b8cfbb30dc3080a8bb073d07abf20a693ae9
horsefacts <webmaster@terminally.online>
2023-02-07 11:55:29 -0500

    Basic scaffold for tests

--- test/WETH9.invariants.t.sol
+++ test/WETH9.invariants.t.sol
@@ -16,55 +16,9 @@ contract WETH9Invariants is Test, InvariantTest {
     function setUp() public {
         weth = new WETH9();
         handler = new Handler(weth);
-
-        bytes4[] memory selectors = new bytes4[](6);
-        selectors[0] = Handler.deposit.selector;
-        selectors[1] = Handler.withdraw.selector;
-        selectors[2] = Handler.sendETH.selector;
-        selectors[3] = Handler.forceSend.selector;
-        selectors[4] = Handler.transfer.selector;
-        selectors[5] = Handler.transferFrom.selector;
-
-        targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
-        excludeContract(address(weth));
-        excludeSender(address(weth));
-        excludeSender(address(this));
     }
 
-    // ETH can only be wrapped into WETH, WETH can only be
-    // unwrapped back into ETH. The sum of Handler's
-    // ETH balance plus the WETH totalSupply() should always
-    // equal the total ETH_SUPPLY.
-    function invariant_conservationOfETH() public {
-        assertEq(handler.ETH_SUPPLY(), address(handler).balance + weth.totalSupply());
-    }
-
-    // WETH balance should always be at least as much as
-    // the sum of individual deposits.
-    function invariant_WETHSolvency_deposits() public {
-        assertEq(
-            weth.totalSupply(), handler.ghost_depositSum() + handler.ghost_forceSendSum() - handler.ghost_withdrawSum()
-        );
-    }
-
-    // WETH balance should always be at least as much as
-    // the sum of individual balances.
-    function invariant_WETHSolvency_balances() public {
-        uint256 sumOfBalances = handler.reduceActors(0, this.accumulateBalance);
-        assertEq(weth.totalSupply(), sumOfBalances + handler.ghost_forceSendSum());
-    }
-
-    function accumulateBalance(uint256 balance, address caller) external view returns (uint256) {
-        return balance + weth.balanceOf(caller);
-    }
-
-    // No individual depositor's balance can exceed the
-    // WETH totalSupply().
-    function invariant_depositorBalances() public {
-        handler.mapActors(this.assertCallerBalanceLteTotalSupply);
-    }
-
-    function assertCallerBalanceLteTotalSupply(address caller) external {
-        assertLe(weth.balanceOf(caller), weth.totalSupply());
+    function invariant_badInvariantThisShouldFail() public {
+        assertEq(1, weth.totalSupply());
     }
 }
