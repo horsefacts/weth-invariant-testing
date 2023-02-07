c6f90a555b2ee38a2ff7e0ae6970381b157dd3e0
horsefacts <webmaster@terminally.online>
2023-02-07 15:03:03 -0500

    Conservation of ETH invariant

--- test/WETH9.invariants.t.sol
+++ test/WETH9.invariants.t.sol
@@ -2,23 +2,29 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
-import {InvariantTest} from "forge-std/InvariantTest.sol";
-import "forge-std/console.sol";
-
-import {Handler} from "./handlers/Handler.sol";
 import {WETH9} from "../src/WETH9.sol";
 
-contract WETH9Invariants is Test, InvariantTest {
-    WETH9 public weth;
+import {Handler, ETH_SUPPLY} from "./handlers/Handler.sol";
 
+contract WETH9Invariants is Test {
+    WETH9 public weth;
     Handler public handler;
 
     function setUp() public {
         weth = new WETH9();
-        handler = new Handler(weth);
+        handler = new Handler(weth); 
+
+        excludeContract(address(weth));
     }
 
-    function invariant_badInvariantThisShouldFail() public {
-        assertEq(1, weth.totalSupply());
+    // ETH can only be wrapped into WETH, WETH can only
+    // be unwrapped back into ETH. The sum of the Handler's
+    // ETH balance plus the WETH totalSupply() should always
+    // equal the total ETH_SUPPLY.
+    function invariant_conservationOfETH() public {
+        assertEq(
+          ETH_SUPPLY, 
+          address(handler).balance + weth.totalSupply()
+        );
     }
 }
--- test/handlers/Handler.sol
+++ test/handlers/Handler.sol
@@ -4,10 +4,32 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {WETH9} from "../../src/WETH9.sol";
 
+uint256 constant ETH_SUPPLY = 120_500_000;
+
 contract Handler is Test {
     WETH9 public weth;
 
     constructor(WETH9 _weth) {
         weth = _weth;
+        deal(address(this), ETH_SUPPLY);
     }
+
+    function deposit(uint256 amount) public {
+        amount = bound(amount, 0, address(this).balance); 
+        weth.deposit{ value: amount }();
+    }
+    
+    function withdraw(uint256 amount) public {
+        amount = bound(amount, 0, weth.balanceOf(address(this))); 
+        weth.withdraw(amount);
+    }
+
+    function sendFallback(uint256 amount) public {
+        amount = bound(amount, 0, address(this).balance);
+        (bool success,) = address(weth).call{ value: amount }("");
+        require(success, "sendFallback failed");
+    }
+
+    receive() external payable {}
+
 }
