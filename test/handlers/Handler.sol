872eddcd5cbf8c48c5733242f0859ada313c4b80
horsefacts <webmaster@terminally.online>
2023-02-07 23:13:29 -0500

    Add bugs and Makefile

--- test/handlers/Handler.sol
+++ test/handlers/Handler.sol
@@ -2,47 +2,10 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {AddressSet, LibAddressSet} from "../helpers/AddressSet.sol";
 import {WETH9} from "../../src/WETH9.sol";
 
-uint256 constant ETH_SUPPLY = 120_500_000;
-
-struct AddressSet {
-    address[] addrs;
-    mapping(address => bool) saved;
-}
-
-library LibAddressSet {
-    function add(AddressSet storage s, address addr) internal {
-        if (!s.saved[addr]) {
-            s.addrs.push(addr);
-            s.saved[addr] = true;
-        }
-    }
-
-    function contains(AddressSet storage s, address addr) internal view returns (bool) {
-        return s.saved[addr];
-    }
-
-    function count(AddressSet storage s) internal view returns (uint256) {
-        return s.addrs.length;
-    }
-
-    function forEach(AddressSet storage s, function(address) external func) internal {
-        for (uint256 i; i < s.addrs.length; ++i) {
-            func(s.addrs[i]);
-        }
-    }
-
-    function reduce(AddressSet storage s, uint256 acc, function(uint256,address) external returns (uint256) func)
-        internal
-        returns (uint256)
-    {
-        for (uint256 i; i < s.addrs.length; ++i) {
-            acc = func(acc, s.addrs[i]);
-        }
-        return acc;
-    }
-}
+uint256 constant ETH_SUPPLY = 120_500_000 ether;
 
 contract Handler is Test {
     using LibAddressSet for AddressSet;
