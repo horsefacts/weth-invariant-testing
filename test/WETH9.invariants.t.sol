a57a6c6592b0a567de9cf992dec64d1d9a43022f
horsefacts <webmaster@terminally.online>
2023-07-01 12:00:26 -0400

    Workshop: test setup

--- test/WETH9.invariants.t.sol
+++ test/WETH9.invariants.t.sol
@@ -1,72 +1,22 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {WETH9} from "../src/WETH9.sol";
 
-import {Handler, ETH_SUPPLY} from "./handlers/Handler.sol";
-
 contract WETH9Invariants is Test {
     WETH9 public weth;
-    Handler public handler;
 
     function setUp() public {
         weth = new WETH9();
-        handler = new Handler(weth);
-
-        bytes4[] memory selectors = new bytes4[](6);
-        selectors[0] = Handler.deposit.selector;
-        selectors[1] = Handler.withdraw.selector;
-        selectors[2] = Handler.sendFallback.selector;
-        selectors[3] = Handler.approve.selector;
-        selectors[4] = Handler.transfer.selector;
-        selectors[5] = Handler.transferFrom.selector;
-        //selectors[6] = Handler.forcePush.selector;
-
-        targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
-
-        targetContract(address(handler));
     }
 
-    // ETH can only be wrapped into WETH, WETH can only
-    // be unwrapped back into ETH. The sum of the Handler's
-    // ETH balance plus the WETH totalSupply() should always
-    // equal the total ETH_SUPPLY.
-    function invariant_conservationOfETH() public {
-        assertEq(ETH_SUPPLY, address(handler).balance + weth.totalSupply());
+    function invariant_wethSupplyIsAlwaysZero() public {
+        assertEq(0, weth.totalSupply());
     }
 
-    // The WETH contract's Ether balance should always be
-    // at least as much as the sum of individual deposits
-    function invariant_solvencyDeposits() public {
-        assertEq(
-            address(weth).balance,
-            handler.ghost_depositSum() + handler.ghost_forcePushSum() - handler.ghost_withdrawSum()
-        );
-    }
-
-    // The WETH contract's Ether balance should always be
-    // at least as much as the sum of individual balances
-    function invariant_solvencyBalances() public {
-        uint256 sumOfBalances = handler.reduceActors(0, this.accumulateBalance);
-        assertEq(address(weth).balance - handler.ghost_forcePushSum(), sumOfBalances);
-    }
-
-    function accumulateBalance(uint256 balance, address caller) external view returns (uint256) {
-        return balance + weth.balanceOf(caller);
-    }
-
-    // No individual account balance can exceed the
-    // WETH totalSupply().
-    function invariant_depositorBalances() public {
-        handler.forEachActor(this.assertAccountBalanceLteTotalSupply);
-    }
-
-    function assertAccountBalanceLteTotalSupply(address account) external {
-        assertLe(weth.balanceOf(account), weth.totalSupply());
-    }
-
-    function invariant_callSummary() public view {
-        handler.callSummary();
+    function test_zeroDeposit() public {
+        weth.deposit{ value: 0 }();
+        assertEq(0, weth.balanceOf(address(this)));
+        assertEq(0, weth.totalSupply());
     }
 }
